04f69e5e1634bf01ee2fc42a451f79f2a4dc3615
Deyvison Vianna <99549533+Deyvinhoo@users.noreply.github.com>
2022-05-10 09:43:48 -0300

    "Realizando tipagem dos elementos"

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,13 +1,14 @@
 export class Negociacao {
-   private _data;
-   private _quantidade;
-   private _valor;
+   private _data: Date;
+   private _quantidade: number;
+   private _valor: number;
 
-   constructor(data, quantidade, valor) {
+   constructor(data: Date, quantidade: number, valor: number) {
       this._data = data;
       this._quantidade = quantidade;
       this._valor = valor;
    }
+
       get data() {
          return this._data;
       };
