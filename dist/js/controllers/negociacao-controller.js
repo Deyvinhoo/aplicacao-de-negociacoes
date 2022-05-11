b2f779dc96c38429c4dbbcc1650bed5b38d514ec
Deyvison Vianna <99549533+Deyvinhoo@users.noreply.github.com>
2022-05-11 16:25:03 -0300

    "Incluindo o metodo limparFormlario e tipando elementos"

--- dist/js/controllers/negociacao-controller.js
+++ dist/js/controllers/negociacao-controller.js
@@ -8,6 +8,7 @@ export class NegociacaoController {
     adiciona() {
         const negociacao = this.criaNegociacao();
         console.log(negociacao);
+        this.limparFormulario();
     }
     criaNegociacao() {
         const exp = /-/g;
@@ -16,4 +17,11 @@ export class NegociacaoController {
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
+    limparFormulario() {
+        this.inputData.value = '';
+        this.inputQuantidade.value = '';
+        this.inputValor.value = '';
+        this.inputData.focus();
+    }
 }
+;
